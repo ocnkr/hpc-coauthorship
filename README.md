6f879babd23379a6e8cabfa1bea8a1b821d942fc
Onur Cankur <ocankur@umd.edu>
2022-03-17 14:37:41 -0400

    add initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# HPC Coauthorsip
+
+Requirements:
+Python 3.4+
+neo4j 3.5, 4.x+
---
+++ src/data_collection.py
@@ -0,0 +1,9 @@
+from neomodel import config
+from lxml import etree
+import requests
+
+# TODO (1): Go to each conference.
+
+
+# TODO (2): Iteratively create author and venue nodes and add papers as relationships (edges).
+# TODO (3): Go to each authors' homepage and look at other papers and do the same.
